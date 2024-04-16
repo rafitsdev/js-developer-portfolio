560f38d3263140d3ef60cfabd8535d801b342d36
Rafael Martins <rrmartins2001@gmail.com>
2024-04-16 17:47:21 -0300

    Updated languages

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,21 +22,28 @@ function updateProfileInfo(profileData) {
   email.href = `email:${profileData.email}`
 }
 
-function updateSoftSkills(profileData) {
-  const softSkills = document.getElementById('profile.skills.softSkills')
-
-  softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
-}
-
 function updateHardSkills(profileData) {
   const hardSkills = document.getElementById('profile.skills.hardSkills')
 
   hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
 }
 
+function updateSoftSkills(profileData) {
+  const softSkills = document.getElementById('profile.skills.softSkills')
+
+  softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+}
+
+function updateLanguages(profileData) {
+  const languages = document.getElementById('profile.languages')
+
+  languages.innerHTML = profileData.languages.map(language => `<li class="languages-item">${language}</li>`).join('')
+}
+
 (async () => {
   const profileData = await fetchProfileData()
   updateProfileInfo(profileData)
-  updateSoftSkills(profileData)
   updateHardSkills(profileData)
+  updateSoftSkills(profileData)
+  updateLanguages(profileData)
 })()
